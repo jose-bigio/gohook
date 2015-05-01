74a7a76e9fa721e6fb4d2c4e63a70c4277fe93e2
M. Cameron Palone <mcameronpalone@gmail.com>
2015-05-01 12:39:49 -0600

    Added test for CommitCommentEvent.

--- gohook_test.go
+++ gohook_test.go
@@ -77,6 +77,30 @@ func TestWatchEvent(t *testing.T) {
 	}
 	_, err = json.Marshal(payload)
 	if err != nil {
-		t.Errorf("Error marshalling PushEvent: %s", err)
+		t.Errorf("Error marshalling WatchEvent: %s", err)
+	}
+}
+
+func TestCommitCommentEvent(t *testing.T) {
+	raw, err := ioutil.ReadFile("testdata/sample_commit_comment.json")
+	if err != nil {
+		t.Errorf("Error reading sample commit_comment file: %s", err)
+	}
+	server := NewServer(8888, "secret", "path")
+	server.processPacket(CommitCommentEventType, raw)
+	packet := <-server.EventAndTypes
+	payload, ok := packet.Event.(*CommitCommentEvent)
+	if !ok {
+		t.Error("Error asserting payload as *CommitCommentEvent.")
+	}
+	if payload.Comment.Body != "This is a really good change! :+1:" {
+		t.Error("Incorrect payload.Comment.Body value.")
+	}
+	if payload.Comment.User.Login != "baxterthehacker" {
+		t.Error("Incorrect payload.Comment.User.Login value.")
+	}
+	_, err = json.Marshal(payload)
+	if err != nil {
+		t.Errorf("Error marshalling CommitCommentEvent: %s", err)
 	}
 }
